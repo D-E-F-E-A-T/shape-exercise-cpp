c6bd84abb3a7f53dd62e2835d0ebe54cd84a1bfb
Richard Bernardino <rmbernardi@gmail.com>
2017-09-09 16:52:50 -0700

    Update Shape.h

--- shape-exercise/Shape.h
+++ shape-exercise/Shape.h
@@ -3,6 +3,10 @@
 class Shape
 {
 public:
+	// This is a pure virtual function
+	//   because it has not default definition.
+	//   Pure virtual function definitions
+	//   always end with "= 0"
 	virtual float CalculateArea() = 0;
 
 protected:
